ea289c408d6069ea45b9db9fdd1825ec5d0609e5
tiagoc0sta <tiagodpv@gmail.com>
2024-05-31 21:18:57 -0400

    Initial Commit

---
+++ 04_ChefDelivery_Alura_SwftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  04_ChefDelivery_Alura_SwftUI
+//
+//  Created by Tiago de Freitas Costa on 2024-05-31.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 04_ChefDelivery_Alura_SwftUI/_4_ChefDelivery_Alura_SwftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  _4_ChefDelivery_Alura_SwftUIApp.swift
+//  04_ChefDelivery_Alura_SwftUI
+//
+//  Created by Tiago de Freitas Costa on 2024-05-31.
+//
+
+import SwiftUI
+
+@main
+struct _4_ChefDelivery_Alura_SwftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
